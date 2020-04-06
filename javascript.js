4953d0d6957cebe771333d6d887cd482f340c8bc
alicerwoodman <61164674+alicerwoodman@users.noreply.github.com>
2020-04-06 22:38:59 +0100

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,8 @@
+var answer = prompt("Want to get weekly updates about women's sport? Answer 'yes' or 'no'.")
+
+if(answer = "yes") {
+    prompt("Leave your email address in the box below! :)") 
+    } 
+if(answer = "no") {
+    alert("Okay, just check out our website instead!")
+    }
